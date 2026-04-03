8afce0f9fea32e668d172dca80a0062be35713d0
fuezal <125521857+fuezal@users.noreply.github.com>
2026-04-03 15:59:18 -0600

    prompt

--- main.py
+++ main.py
@@ -1,32 +1,7 @@
-#Librerias obligatorias
-import os
-from dotenv import load_dotenv
 import streamlit as st
-from openai import OpenAI
+st.title(" 📈 Capital Eye")
+st.caption("Herramienta financiera con IA para inversionistas")
 
-load_dotenv(override=True)
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-
-client_openai = OpenAI(api_key=OPENAI_API_KEY)
-model_openai = "gpt-5.4-mini"
-
-st.title("📊 Capital Eye")
-st.caption("💰Herramienta informativa para inversionistas")
-
-if "messages" not in st.session_state:
-    st.session_state.messages = [{"role": "assistant", "content": "¿En qué te puedo ayudar?"}]
-
-for msg in st.session_state.messages:
-    st.chat_message(msg["role"]).write(msg["content"])
-
-if prompt:= st.chat_input("Escribe tu mensaje aquí..."):
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    st.chat_message("user").write(prompt)
-    conversation = [{"role": "assistant", "content": "Eres un experto en inversiones, finanzas y bolsa de valores. Ayuda al usuario a entender mejor las inversiones y mercado financiero. Responde siempre en español."}]
-    conversation.extend({"role": m["role"], "content": m["content"]} for m in st.session_state.messages)
-
-    with st.chat_message("assistant"):
-        stream = client_openai.chat.completions.create(model=model_openai, messages=conversation, stream=True)
-        response = st.write_stream(stream)
-
-    st.session_state.messages.append({"role": "assistant", "content": response})
+prompt = st.chat_input("¿En que te puedo ayudar?")
+if prompt:
+    st.write("El usuario ha enviado el siguiente mensaje: ", prompt)
